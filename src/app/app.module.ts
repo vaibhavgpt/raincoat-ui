8462c07844fd9025a41573d1e178862de4588f4a
Sachin Gopal <sachin.gopal2015@vit.ac.in>
2019-06-20 19:39:28 +0530

    Get current location

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { LocationComponent } from './location/location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
